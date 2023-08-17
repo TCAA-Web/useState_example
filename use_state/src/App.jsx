5a4be49cb32c159e3c811f20fa653ca1a940ad44
TCAA-Web <kfry@techcollege.dk>
2023-08-17 13:58:49 +0200

    initial

---
+++ use_state/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {  
+
+  const [result, setResult] = useState(0)
+  const [valueOne, setValueOne] = useState(0)
+  const [valueTwo, setValueTwo] = useState(0)
+
+  const add = () => {
+    const res = valueOne + valueTwo
+    setResult(res)
+  }
+  
+  const subtract = () => {
+    const res = valueOne - valueTwo
+    setResult(res)
+  }
+
+  const divide = () => {
+    const res = valueOne / valueTwo
+    setResult(res)
+  }
+
+  const multiply = () => {
+    const res = valueOne * valueTwo
+    setResult(res)
+  }
+
+  console.log(result)
+
+  return (
+    <> 
+      <h3>Result: {result}</h3>
+      <input onChange={(event) => setValueOne(parseInt(event.target.value))} value={valueOne} type="number"></input>
+      <input onChange={(event) => setValueTwo(parseInt(event.target.value))} value={valueTwo} type="number"></input>
+      <button onClick={add}>+</button>
+      <button onClick={subtract}>-</button>
+      <button onClick={multiply}>*</button>
+      <button onClick={divide}>/</button>
+    </>
+  )
+}
+
+export default App
